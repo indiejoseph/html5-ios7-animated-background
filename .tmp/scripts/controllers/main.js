e9f7b77c321cba13e79d3289aa16802330899498
Joseph Cheng <indiejoseph@gmail.com>
2013-07-19 00:08:03 +0800

    Added index.html

---
+++ .tmp/scripts/controllers/main.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+  angular.module('animatedBackgroundApp').controller('MainCtrl', function($scope) {
+    return $scope.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma'];
+  });
+
+}).call(this);
