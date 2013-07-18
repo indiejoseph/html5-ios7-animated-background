e810ed159938f2b19be06b5de35a82eeb062dde1
Joseph Cheng <indiejoseph@gmail.com>
2013-07-19 00:11:04 +0800

    Added gitignore

--- .tmp/scripts/controllers/main.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-  angular.module('animatedBackgroundApp').controller('MainCtrl', function($scope) {
-    return $scope.awesomeThings = ['HTML5 Boilerplate', 'AngularJS', 'Karma'];
-  });
-
-}).call(this);
